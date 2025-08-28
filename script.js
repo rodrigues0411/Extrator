4a87c4a6c33cc079ffa97c7fe27bbe2148319648
Aline J Rodrigues <jesus.rodrigues.aline@escola.pr.gov.br>
2025-08-28 16:49:45 -0300

    function processaTexto

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    campoResultado.textContent = texto;
-    const palavras = texto.split(" ");
+    const palavras = processaTexto (texto);
+
     campoResultado.textContent = palavras.join("- ");
+}
+
+function processaTexto(texto){
+    let palavras = texto.split(/[^a-Za-Z]+/);
+    return palavras;
 }
